7fbce2c66db3f3c09e3810ac582a82d53d7571d9
Cassidy <caws4@illinois.edu>
2018-02-06 16:53:18 -0600

    New changes-- wordCounter done

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -3,26 +3,36 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class WebScraper {
-    public static void main (String[] unused) {
+    public static void main(String[] unused) {
         System.out.println("Ran");
     }
-}
 
-
-/**
- * Retrieve contents from a URL and return them as a string.
- *
- * @param url url to retrieve contents from
- * @return the contents from the url as a string, or an empty string on error
- */
-public static String urlToString(final String url) {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
         Scanner urlScanner;
         try {
-        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
         } catch (IOException e) {
-        return "";
+            return "";
         }
         String contents = urlScanner.useDelimiter("\\A").next();
         urlScanner.close();
         return contents;
+    }
+
+    public static String wordCount (String website ) {
+        String hamlet = urlToString(website);
+        String[] words = hamlet.split("[ ,./+;:\\[\\]\\-\\']+");
+        int wordCounter = 0;
+        for (int i = 0; i < words.length; i++) {
+            wordCounter++;
         }
+        System.out.println(wordCounter);
+        return hamlet;
+    }
+}
